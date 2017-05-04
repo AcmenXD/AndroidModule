fe5c1f6e29639b841ff86a757929d5417bf816a6
wxd_b <wxd199268>
2017-05-04 16:22:23 +0800

    增加混淆基类

---
+++ app/src/main/java/com/acmenxd/mvp/utils/proguard/IKeepClass.java
@@ -0,0 +1,12 @@
+package com.acmenxd.mvp.utils.proguard;
+
+/**
+ * @author AcmenXD
+ * @version v1.0
+ * @github https://github.com/AcmenXD
+ * @date 2017/2/21 18:00
+ * @detail 实现此接口的类 -> 将保持整个类都不混淆
+ */
+public interface IKeepClass extends IKeepFieldName, IKeepMethodName {
+
+}
