9d0535b5a69eeb6b9362da1e3466346de6606c6b
wxd_b <wxd199268>
2017-06-19 18:18:07 +0800

    增加结构包

---
+++ mvp/src/main/java/com/acmenxd/mvp/utils/Utils.java
@@ -0,0 +1,11 @@
+package com.acmenxd.mvp.utils;
+
+/**
+ * @author AcmenXD
+ * @version v1.0
+ * @github https://github.com/AcmenXD
+ * @date 2017/6/19 18:17
+ * @detail 业务工具类
+ */
+public class Utils {
+}
