2464f3702a85da49a9354ca25cdf888160de8741
wxd_b <wxd199268>
2017-11-02 11:11:50 +0800

    整理更新

--- frame/src/main/java/com/acmenxd/retrofit/HttpManager.java
+++ frame/src/main/java/com/acmenxd/retrofit/HttpManager.java
@@ -227,8 +227,7 @@ public enum HttpManager {
          */
         SSLContext sslContext = null;
         try {
-//            sslContext = SSLContext.getInstance("TLS");
-            sslContext = SSLContext.getInstance("SSL");
+            sslContext = SSLContext.getInstance("SSL"); // SSLContext.getInstance("TLS");
             sslContext.init(null, new TrustManager[]{new X509TrustManager() {
                 @Override
                 public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
